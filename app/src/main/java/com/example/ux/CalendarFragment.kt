a9a7b317c9b23b1814b90523708d54df611e988b
jiheunkim <ziheun54@gmail.com>
2023-11-20 10:50:17 +0900

    Feat: 캘린더 상단메뉴바 - ScheduleFragment - DeadlineFragment

--- app/src/main/java/com/example/ux/CalendarFragment.kt
+++ app/src/main/java/com/example/ux/CalendarFragment.kt
@@ -1,59 +1,77 @@
 package com.example.ux
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ux.databinding.FragmentCalendarBinding
+import com.google.android.material.tabs.TabLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CalendarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CalendarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var binding: FragmentCalendarBinding
+    lateinit var mContext: Context
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalendarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toolbar()
+    }
+
+    private fun Toolbar() {
+        val tabLayout = binding.friendTabs
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val tabPosition = tab?.position
+
+                tab?.icon?.setColorFilter(Color.parseColor("#002AFF"), PorterDuff.Mode.SRC_IN)
+
+                when (tabPosition) {
+                    0 -> {
+                        val scheduleFragment = ScheduleFragment()
+                        requireActivity().supportFragmentManager.beginTransaction()
+                            .replace(R.id.friend_container, scheduleFragment).commit()
+                    }
+                    1 -> {
+                        val deadlineFragment = DeadlineFragment()
+                        requireActivity().supportFragmentManager.beginTransaction()
+                            .replace(R.id.friend_container, deadlineFragment).commit()
+                    }
                 }
             }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // icon color black으로
+                tab?.icon?.setColorFilter(Color.parseColor("#E0E0E0"), PorterDuff.Mode.SRC_IN)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                //구현하지 않음
+            }
+
+        })
+
+        // Set default fragment
+        val scheduleFragment = ScheduleFragment()
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.friend_container, scheduleFragment).commit()
+
+        tabLayout.getTabAt(0)?.icon?.setColorFilter(Color.parseColor("#002AFF"), PorterDuff.Mode.SRC_IN)
     }
 }
