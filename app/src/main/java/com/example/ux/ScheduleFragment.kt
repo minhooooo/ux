4ce54cf48ec438d25307ad58c5c6d9de26a69875
minhooooo <andy990401@gmail.com>
2023-11-22 10:54:39 +0900

    sorrry

--- app/src/main/java/com/example/ux/ScheduleFragment.kt
+++ app/src/main/java/com/example/ux/ScheduleFragment.kt
@@ -1,10 +1,10 @@
 package com.example.ux
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +35,10 @@ class ScheduleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_schedule, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     companion object {
