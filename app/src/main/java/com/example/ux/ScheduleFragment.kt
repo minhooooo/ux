842b07e9a0871461f1f1fbef453cb2ba38b9b078
minhooooo <andy990401@gmail.com>
2023-11-24 17:42:37 +0900

    done schedule fragment

--- app/src/main/java/com/example/ux/ScheduleFragment.kt
+++ app/src/main/java/com/example/ux/ScheduleFragment.kt
@@ -46,9 +46,11 @@ class ScheduleFragment : Fragment() {
         Log.d("schedule", uid.toString())
 
         var currentTime = LocalDateTime.now();
-        Log.d("dateTest", "1) 현재시간::  " + currentTime)
+
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val formattedDate = currentTime.format(formatter)
+
+        Log.d("dateTest", "현재 날짜: " + formattedDate)
 
         var currentweek = week(formattedDate)
 
@@ -108,7 +110,7 @@ class ScheduleFragment : Fragment() {
                 }
                 clickedTextViewIds.clear()
 
-                changeweek(tempweek,-7)
+                tempweek=changeweek(tempweek,-7)
 
                 // UI 업데이트
                 nowYear.setText("${tempweek[0]}년")
@@ -125,7 +127,7 @@ class ScheduleFragment : Fragment() {
                 }
                 clickedTextViewIds.clear()
 
-                changeweek(tempweek,7)
+                tempweek=changeweek(tempweek,7)
                 // UI 업데이트
                 nowYear.setText("${tempweek[0]}년")
                 nowWeek.setText("${tempweek[1]}월${tempweek[2]}일 ~ ${tempweek[4]}월${tempweek[5]}일")
@@ -235,21 +237,21 @@ class ScheduleFragment : Fragment() {
         val startDtArray = startDt.split("-").toTypedArray()
         val endDtArray = endDt.split("-").toTypedArray()
 
-        Log.d("shedule", (startDtArray + endDtArray).toString())
+        Log.d("schedule", startDtArray.joinToString(",")+"~"+endDtArray.joinToString(","))
 
         return startDtArray + endDtArray
     }
 
     private fun changeweek(tempweek:Array<String>,num:Int):Array<String>{
         // 현재 주의 시작 날짜를 기반으로 계산
-        val cal = Calendar.getInstance()
+        var cal = Calendar.getInstance()
         cal.set(tempweek[0].toInt(), tempweek[1].toInt() - 1, tempweek[2].toInt()) // 현재 주의 시작 날짜 설정
         cal.add(Calendar.DAY_OF_MONTH, num)
 
         // 새로운 주의 날짜로 tempweek 업데이트
-        val newYear = cal.get(Calendar.YEAR).toString()
-        val newMonth = (cal.get(Calendar.MONTH) + 1).toString() // 월은 0부터 시작하므로 1 더함
-        val newDay = cal.get(Calendar.DAY_OF_MONTH).toString()
+        var newYear = cal.get(Calendar.YEAR).toString()
+        var newMonth = (cal.get(Calendar.MONTH) + 1).toString() // 월은 0부터 시작하므로 1 더함
+        var newDay = cal.get(Calendar.DAY_OF_MONTH).toString()
 
         // tempweek 업데이트
         tempweek[0] = newYear
@@ -259,9 +261,16 @@ class ScheduleFragment : Fragment() {
         cal.set(tempweek[3].toInt(), tempweek[4].toInt() - 1, tempweek[5].toInt()) // 현재 주의 시작 날짜 설정
         cal.add(Calendar.DAY_OF_MONTH, num)
 
+        newYear = cal.get(Calendar.YEAR).toString()
+        newMonth = (cal.get(Calendar.MONTH) + 1).toString() // 월은 0부터 시작하므로 1 더함
+        newDay = cal.get(Calendar.DAY_OF_MONTH).toString()
+
         tempweek[3] = newYear
         tempweek[4] = newMonth
         tempweek[5] = newDay
+
+        Log.d("schedule", tempweek.joinToString(","))
+
         return tempweek
     }
     override fun onDestroyView() {
