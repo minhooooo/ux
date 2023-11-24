af0fb3f5f7fbaffbd9147d38ce37bf515adc9d25
minhooooo <andy990401@gmail.com>
2023-11-24 16:00:50 +0900

    week data

--- app/src/main/java/com/example/ux/ScheduleFragment.kt
+++ app/src/main/java/com/example/ux/ScheduleFragment.kt
@@ -2,7 +2,6 @@ package com.example.ux
 
 import android.content.Context
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Bundle
@@ -50,18 +49,22 @@ class ScheduleFragment : Fragment() {
         Log.d("dateTest", "1) 현재시간::  " + currentTime)
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val formattedDate = currentTime.format(formatter)
-        week(formattedDate)
+
+        var currentweek = week(formattedDate)
+
         // Firebase에서 데이터 로드
-        loadDataFromFirebase(uid!!)
-        onUserInteractionChangedData()
+        loadDataFromFirebase(uid!!,currentweek)
+        onUserInteractionChangedData(currentweek)
         return binding.root
 
     }
 
 
-    private fun loadDataFromFirebase(uid: String) {
+    private fun loadDataFromFirebase(uid: String, currentweek:Array<String>) {
         val db = Firebase.database.getReference("moi")
         val possibleRef = db.child(uid).child("timeTable").child("possible")
+            .child(currentweek[0]).child(currentweek[1]).child(currentweek[2])
+
 
         possibleRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -91,8 +94,45 @@ class ScheduleFragment : Fragment() {
     }
 
     // 사용자 인터랙션에 의한 데이터 변경
-    fun onUserInteractionChangedData() {
+    fun onUserInteractionChangedData(currentweek: Array<String>) {
         binding.apply {
+            var tempweek=currentweek
+            nowYear.setText("${tempweek[0]}년")
+            nowWeek.setText("${tempweek[1]}월${tempweek[2]}일 ~ ${tempweek[4]}월${tempweek[5]}일")
+            lastWeek.setOnClickListener {
+                clickedTextViewIds.forEach { textViewIdName ->
+                    val resId = resources.getIdentifier(textViewIdName, "id", requireContext().packageName)
+                    val textView = binding.root.findViewById<TextView>(resId)
+                    textView.background = ContextCompat.getDrawable(requireContext(), R.drawable.cell_normal)
+                    textView.setTag("cell_normal") // 태그 설정
+                }
+                clickedTextViewIds.clear()
+
+                changeweek(tempweek,-7)
+
+                // UI 업데이트
+                nowYear.setText("${tempweek[0]}년")
+                nowWeek.setText("${tempweek[1]}월${tempweek[2]}일 ~ ${tempweek[4]}월${tempweek[5]}일")
+
+                loadDataFromFirebase(uid!!,tempweek)
+            }
+            nextWeek.setOnClickListener {
+                clickedTextViewIds.forEach { textViewIdName ->
+                    val resId = resources.getIdentifier(textViewIdName, "id", requireContext().packageName)
+                    val textView = binding.root.findViewById<TextView>(resId)
+                    textView.background = ContextCompat.getDrawable(requireContext(), R.drawable.cell_normal)
+                    textView.setTag("cell_normal") // 태그 설정
+                }
+                clickedTextViewIds.clear()
+
+                changeweek(tempweek,7)
+                // UI 업데이트
+                nowYear.setText("${tempweek[0]}년")
+                nowWeek.setText("${tempweek[1]}월${tempweek[2]}일 ~ ${tempweek[4]}월${tempweek[5]}일")
+
+                loadDataFromFirebase(uid!!,tempweek)
+            }
+
             var isCreate = true
 
             create.setOnClickListener {
@@ -130,21 +170,23 @@ class ScheduleFragment : Fragment() {
                 }
                 else {
                     // 현재 'apply' 상태인 경우, 'create' 상태로 변경
-                    create.text = "등록"
+                    create.text = "수정"
                     create.setBackgroundResource(R.drawable.btn_circle_gray) // 'create' 상태의 배경
                     create.setTextColor(Color.parseColor("#787878")) // 'create' 상태의 텍스트 색상
 
                     //gridlayout에 색이 바뀐부분 db저장
-                    saveDataToFirebase()
+                    saveDataToFirebase(currentweek)
                 }
                 isCreate = !isCreate // 상태 토글
             }
         }
     }
-    private fun saveDataToFirebase() {
+    private fun saveDataToFirebase(currentweek: Array<String>) {
+
         if (isNetworkAvailable()) {
             val db = Firebase.database.getReference("moi")
             val possibleRef = db.child(uid!!).child("timeTable").child("possible")
+                .child(currentweek[0]).child(currentweek[1]).child(currentweek[2])
             possibleRef.setValue(clickedTextViewIds)
         } else {
             // TODO: 네트워크가 사용 불가능할 때
@@ -166,20 +208,61 @@ class ScheduleFragment : Fragment() {
         }
         return false
     }
-    private fun week(eventDate: String) {
+    private fun week(eventDate: String): Array<String> {
         val dateArray = eventDate.split("-").toTypedArray()
 
-        val year = dateArray[0].toInt()
-        val month = dateArray[1].toInt()
-        val day = dateArray[2].toInt()
-
         val cal = Calendar.getInstance()
-        cal.set(Calendar.YEAR, year)
-        cal.set(Calendar.MONTH, month - 1) // Calendar.MONTH는 0부터 시작합니다. 예: 0은 1월, 1은 2월, ...
-        cal.set(Calendar.DAY_OF_MONTH, day)
-        val weekOfMonth = cal.get(Calendar.WEEK_OF_MONTH)
-        Log.d("scheduls", "EventDate: $year 년 $month 월 $weekOfMonth 번째주")
+        cal[dateArray[0].toInt(), dateArray[1].toInt() - 1] = dateArray[2].toInt()
 
+        // 일주일의 첫날을 일요일
+        cal.firstDayOfWeek = Calendar.SUNDAY
+
+        // 시작일과 특정날짜의 차이
+        val dayOfWeek = cal[Calendar.DAY_OF_WEEK] - cal.firstDayOfWeek
+
+        // 해당 주차의 첫째날을 지정
+        cal.add(Calendar.DAY_OF_MONTH, -dayOfWeek)
+
+        val sf = SimpleDateFormat("yyyy-MM-dd")
+
+        // 해당 주차의 첫째 날짜
+        val startDt = sf.format(cal.time)
+
+        cal.add(Calendar.DAY_OF_MONTH, 6)
+
+        val endDt = sf.format(cal.time)
+
+        val startDtArray = startDt.split("-").toTypedArray()
+        val endDtArray = endDt.split("-").toTypedArray()
+
+        Log.d("shedule", (startDtArray + endDtArray).toString())
+
+        return startDtArray + endDtArray
+    }
+
+    private fun changeweek(tempweek:Array<String>,num:Int):Array<String>{
+        // 현재 주의 시작 날짜를 기반으로 계산
+        val cal = Calendar.getInstance()
+        cal.set(tempweek[0].toInt(), tempweek[1].toInt() - 1, tempweek[2].toInt()) // 현재 주의 시작 날짜 설정
+        cal.add(Calendar.DAY_OF_MONTH, num)
+
+        // 새로운 주의 날짜로 tempweek 업데이트
+        val newYear = cal.get(Calendar.YEAR).toString()
+        val newMonth = (cal.get(Calendar.MONTH) + 1).toString() // 월은 0부터 시작하므로 1 더함
+        val newDay = cal.get(Calendar.DAY_OF_MONTH).toString()
+
+        // tempweek 업데이트
+        tempweek[0] = newYear
+        tempweek[1] = newMonth
+        tempweek[2] = newDay
+
+        cal.set(tempweek[3].toInt(), tempweek[4].toInt() - 1, tempweek[5].toInt()) // 현재 주의 시작 날짜 설정
+        cal.add(Calendar.DAY_OF_MONTH, num)
+
+        tempweek[3] = newYear
+        tempweek[4] = newMonth
+        tempweek[5] = newDay
+        return tempweek
     }
     override fun onDestroyView() {
         super.onDestroyView()
